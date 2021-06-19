a321eac6e31fc078b55b8d16454645ffe48fc1ca
AustinJoo97 <austinjoo97@gmail.com>
2021-06-19 00:57:29 -0400

    working on workout model; completed index model; created html and api route files; completed html serving js files

--- server.js
+++ server.js
@@ -2,20 +2,19 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(logger("dev"));
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
